29b0d5775a81b416c69f92e8b46c636f1313fd9e
Josh Kolenko <joshua.kolenko@wwwinc.com>
2023-09-26 11:21:47 -0400

    feat(example): add Sass loadPath use case

--- example/index.ts
+++ example/index.ts
@@ -1,20 +1,31 @@
 import fs from 'fs';
 import path from 'path';
 
-import { inlineHTML } from '../src/index.js';
+import { inlineHTML } from '../dist/index.js';
 
 (async () => {
   try {
     const htmlPath = path.resolve('example/src/index.html');
-    const htmlStr = fs.readFileSync(
-      path.resolve('example/src/index.html'),
-      'utf8'
-    );
+    const htmlStr = fs.readFileSync(path.resolve('example/src/index.html'), 'utf8');
 
-    const pathInlined = await inlineHTML(htmlPath);
-    const strInlined = await inlineHTML(htmlStr, { dir: 'example/src' });
+    const loadPaths = ['example/sass/load/path'];
 
-    console.log({ pathInlined, strInlined });
+    const html = {
+      path: await inlineHTML(htmlPath, { loadPaths }),
+      str: await inlineHTML(htmlStr, { dir: 'example/src', loadPaths }),
+    };
+
+    const distPath = path.resolve('example/dist');
+
+    if (!fs.existsSync(distPath)) {
+      fs.mkdirSync(distPath);
+    } else {
+      fs.rmSync(distPath, { recursive: true });
+      fs.mkdirSync(distPath);
+    }
+
+    fs.writeFileSync(path.join(distPath, 'path.html'), html.path);
+    fs.writeFileSync(path.join(distPath, 'str.html'), html.str);
   } catch (error) {
     console.log(error);
   }
